0a7798c475763b7a4baffb8a0f9a6f5bbf642418
Nazar Lipkovskyy <nazar.lipkovskyy@gmail.com>
2019-07-01 01:16:53 +0300

    Add profiles usage

--- src/main/java/com/nazjara/services/SpanishGreetingService.java
+++ src/main/java/com/nazjara/services/SpanishGreetingService.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile({"en", "default"})
+@Profile("es")
 @Primary
-public class PrimaryGreetingService implements GreetingService {
+public class SpanishGreetingService implements GreetingService {
 
-    private static final String HELLO_WORLD = "Hello World from PrimaryGreetingService!";
+    private static final String HELLO_WORLD = "Hola mundo desde el servicio de saludo español!";
 
     @Override
     public String sayGreeting() {
